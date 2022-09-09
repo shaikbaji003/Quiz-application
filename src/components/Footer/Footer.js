88dcdcfae11d1ce6192ae658ca77b85a67b2730e
SHAIK BAJI <92388095+shaikbaji003@users.noreply.github.com>
2022-09-09 19:50:14 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <div
+      style={{
+        textAlign: "center",
+        marginBottom: 10,
+      }}
+    >
+      Made by{" "}
+      <a
+        href="https://github.com/shaikbaji003?tab=repositories"
+        style={{ cursor: "pointer" }}
+      >
+        Baji
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
